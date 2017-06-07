172b2482b4ebc61789aacd61b577a48ecc3b3d10
joyshao37 <102306001@nccu.edu.tw>
2017-06-07 21:39:27 +0800

    like button

--- public/js/article.js
+++ public/js/article.js
@@ -9,6 +9,7 @@ var map = document.querySelector('iframe');
 var like = document.querySelector('#like');
 var map_url = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyBy5JYq1_BVxB1A2Xtofn9PcRryxQJ4Ess';
 var c,id;
+var userid = 'joy_shao37'; //Current User
 
 //Get the post ID (parameter) passed from search.html
 var query = location.search.substring(1);
@@ -68,22 +69,43 @@ postRef.on('value', function(snapshot) {
 
 
 document.querySelector(".likes-count").addEventListener('click',function(e){
-   this.classList.toggle("like-click") ;
+   //this.classList.toggle("like-click") ;
 
-   console.log(c+"="+id);
-   
+      var likes = like.innerHTML;
+      var likeRef = firebase.database().ref('Post/'+c+'/'+id);
+
+      likeRef.once('value',function(snapshot){
+
+            var count = snapshot.val().like_count;
+
+            //如果有likes欄位->已經有人按讚、如果likes欄位有使用者的ID -> 已經按過讚了
+            if(snapshot.child('like_user').exists()||snapshot.child('like_user').hasChild(userid)){
+
+                postRef.child(c).child(id).child('like_user').child(userid).remove();
+                count--;
+                postRef.child(c).child(id).child('like_count').set(count);
+                              
+            }else{
+
+                postRef.child(c).child(id).child('like_user').child(userid).set(true);
+                count++;
+                postRef.child(c).child(id).child('like_count').set(count);
+
+               }
 
 
-
-
-
-
-
-
-
+               //改變btn上的讚數
+               like.innerHTML = count;      
+               
+      });
 
 
 
 
    
 });
+
+
+
+
+
