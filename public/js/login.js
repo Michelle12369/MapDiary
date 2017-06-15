93a330c849da68b7bd8ab53ba7525ec25ba936e0
leo <aleoliu566@gmail.com>
2017-06-15 10:00:50 +0800

    user page時間排序、刪除按鈕

--- public/js/login.js
+++ public/js/login.js
@@ -5,40 +5,50 @@ var database = firebase.database();
 var accountL = document.getElementById("accountL");
 var pwdL = document.getElementById("pwdL");
 var loginSmtBtn = document.getElementById("loginSmtBtn");
-loginSmtBtn.addEventListener("click",function(){
-  console.log(accountL.value);
-  firebase.auth().signInWithEmailAndPassword(accountL.value, pwdL.value).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage);
-  })
-},false);
+if(loginSmtBtn!=null){
+  loginSmtBtn.addEventListener("click",function(){
+    console.log(accountL.value);
+    firebase.auth().signInWithEmailAndPassword(accountL.value, pwdL.value).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    })
+  },false);
+}
 
 
 //Email驗證
 var verifyBtn = document.getElementById("verifyBtn");
-verifyBtn.addEventListener("click",function(){
-  user.sendEmailVerification().then(function() {
-    console.log("驗證信寄出");
-  }, function(error) {
-    console.error("驗證信錯誤");
-  });
-},false);
+if(verifyBtn!=null){
+  verifyBtn.addEventListener("click",function(){
+    user.sendEmailVerification().then(function() {
+      console.log("驗證信寄出");
+    }, function(error) {
+      console.error("驗證信錯誤");
+    });
+  },false);
+}
 
 //更改密碼
 var chgPwd = document.getElementById("chgPwd");
-var chgPwdBtn = document.getElementById("chgPwdBtn");
-chgPwdBtn.addEventListener("click",function(){
-  firebase.auth().sendPasswordResetEmail(chgPwd.value).then(function() {
-    // Email sent.
-    console.log("更改密碼Email已發送");
-    chgPwd.value = "";
-  }, function(error) {
-    // An error happened.
-    console.error("更改密碼",error);
-  });
-},false);
+if(chgPwdBtn != null){
+  var chgPwdBtn = document.getElementById("chgPwdBtn");
+  chgPwdBtn.addEventListener("click",function(){
+    firebase.auth().sendPasswordResetEmail(chgPwd.value).then(function() {
+      // Email sent.
+      console.log("更改密碼Email已發送");
+      chgPwd.value = "";
+    }, function(error) {
+      // An error happened.
+      console.error("更改密碼",error);
+    });
+  },false);
+}
 
 
-
+firebase.auth().onAuthStateChanged(function(user) {
+  if(user !=null){
+    document.location.href='/index.html';
+  }
+});
